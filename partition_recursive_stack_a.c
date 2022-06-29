20e3a0300002e07699942d5e0e7a524e49185b14
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-06-29 12:36:13 +0900

    자잘한 수정2

--- partition_recursive_stack_a.c
+++ partition_recursive_stack_a.c
@@ -1,7 +1,5 @@
 # include "push_swap.h"
 
-/* partition_recursive_stack_a */
-
 static void	base_case_solve(t_pushswap *pushswap, size_t len)
 {
 	if (len == 2 && stack_is_ascending_order(pushswap->stack_a, len) == false)
@@ -28,20 +26,8 @@ static void	chuck_move_to_top(t_pushswap *pushswap, size_t pivot1, size_t pivot2
 	}
 }
 
-void	partition_recursive_stack_a(t_pushswap *pushswap, size_t left, size_t right)
+static void	partition(t_pushswap *pushswap, size_t pivot1, size_t pivot2, size_t len)
 {
-	size_t	pivot1;
-	size_t	pivot2;
-	size_t	len;
-
-	len = right - left + 1;
-	if (len <= 2)
-	{
-		base_case_solve(pushswap, len);
-		return ;
-	}
-	pivot1 = left + (len / 3);
-	pivot2 = left + (len / 3 * 2);
 	while (len--)
 	{
 		if (pushswap->ordered_arr[pivot2] <= stack_peek(pushswap->stack_a))
@@ -54,8 +40,27 @@ void	partition_recursive_stack_a(t_pushswap *pushswap, size_t left, size_t right
 		else
 			pb(pushswap->stack_a, pushswap->stack_b);
 	}
-	chuck_move_to_top(pushswap, pivot1, pivot2, right);
-	partition_recursive_stack_a(pushswap, pivot2, right);
-	partition_recursive_stack_b(pushswap, pivot1, pivot2 - 1);
-	partition_recursive_stack_b(pushswap, left, pivot1 - 1);
+}
+
+void	partition_recursive_stack_a(t_pushswap *pushswap, size_t left, size_t right)
+{
+	size_t	pivot1;
+	size_t	pivot2;
+	size_t	len;
+
+	len = right - left + 1;
+	if (len <= 2)
+		base_case_solve(pushswap, len);
+	else
+	{
+		pivot1 = left + (len / 3);
+		pivot2 = left + (len / 3 * 2);
+
+		partition(pushswap, pivot1, pivot2, len);
+		chuck_move_to_top(pushswap, pivot1, pivot2, right);
+
+		partition_recursive_stack_a(pushswap, pivot2, right);
+		partition_recursive_stack_b(pushswap, pivot1, pivot2 - 1);
+		partition_recursive_stack_b(pushswap, left, pivot1 - 1);
+	}
 }
